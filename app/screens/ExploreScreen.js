b3a327e4a7de18701e4e968a5953bce860b603a4
Alvaro-Hernandez <72538772+Alvaro-Hernandez@users.noreply.github.com>
2021-04-06 12:25:57 -0500

    Scroll

--- app/screens/ExploreScreen.js
+++ app/screens/ExploreScreen.js
@@ -1,24 +1,88 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Image, ScrollView, Dimensions, Text, StyleSheet} from 'react-native';
 
-const ExploreScreen = () => {
-    return (
-      <View style={styles.container}>
-        <Text>ExploreScreen</Text>
-        <Button
-          title="Click Here"
-          onPress={() => alert('Button Clicked!')}
-        />
+const {width} = Dimensions.get("window");
+const height = width * 0.6; //60%
+
+const images = [
+  'https://image.freepik.com/vector-gratis/concepto-violencia-genero_23-2148607773.jpg',
+  'https://image.freepik.com/vector-gratis/detener-concepto-violencia-genero_23-2148581693.jpg',
+  'https://image.freepik.com/foto-gratis/violencia-fisica-domestica_155003-26348.jpg',
+  'https://image.freepik.com/foto-gratis/mano-nina-oscuridad_127220-3.jpg',
+  'https://image.freepik.com/foto-gratis/nina-triste-asustada-ojos-inyectados-sangre-amoratados-falsa-sonrisa-boca_155003-27671.jpg'
+]
+
+export default class ExploreScreen extends React.Component{
+  state ={
+      active: 0
+  }
+
+  change =({nativeEvent})=>{
+    const slide = Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width);
+    if(slide !== this.state.active){
+      this.setState({active: slide});
+    }  
+  }
+  render(){
+    return(
+      <View styles={style.container}>
+        <ScrollView 
+        pagingEnabled 
+        horizontal
+        onScroll={this.change}
+        showsHorizontalScrollIndicator={false} 
+        style={style.scroll}
+        >
+        {
+          images.map((image, index) => (
+            <Image
+            key={index}
+              source={{ uri: image}}
+              style={style.image} />
+          ))
+        }
+        </ScrollView>
+        <View style={style.pagination}>
+          {
+            images.map((i, k) => (
+              <Text key={k} style={k==this.state.active ? style.imageActiveBotton : style.imageBotton}>⬤</Text>
+            ))
+          }
+        </View>
       </View>
-    );
-};
+    )
+  }
+}
 
-export default ExploreScreen;
-
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    marginTop: 50, 
+    width, 
+    height 
   },
-});
+  scroll:{
+    width, 
+    height
+  },
+  image: { 
+    width, 
+    height, 
+    resizeMode: 'cover'
+  },
+  pagination: {
+    flexDirection:'row', 
+    position: 'absolute', 
+    bottom:0, 
+    alignSelf: 'center'
+  },
+  imageBotton: {
+    fontSize:(width / 30), 
+    color:'#888', 
+    margin: 3
+  },
+  imageActiveBotton: {
+    fontSize:(width / 30), 
+    color:'#fff', 
+    margin: 3
+  }
+})
